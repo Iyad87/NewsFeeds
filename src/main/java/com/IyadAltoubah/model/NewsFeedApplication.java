07e56bc1ce76cf899b3779c7a9cb5cfb58d098c5
iyad87 <iyad0tata@gmail.com>
2020-11-05 18:12:43 +0100

    setting up spring-boot project

---
+++ src/main/java/com/IyadAltoubah/model/NewsFeedApplication.java
@@ -0,0 +1,18 @@
+package com.IyadAltoubah.model;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsFeedApplication {
+
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(NewsFeedApplication.class,args);
+
+    }
+
+}
